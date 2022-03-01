5e325fb9ed97d2e1b270d0f911745c1a858ae948
carolsouza <caah.souza@live.com>
2022-02-28 23:19:37 -0300

    Início do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import Modal from "react-modal";
 
-import { GlobalStyle } from './styles/global';
-
-
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header /> 
-      <Dashboard />     
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
 }
-
